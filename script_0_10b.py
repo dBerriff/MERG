98626405d932323d9bc321f177df21b49f33db7f
dBerriff <berriff@icloud.com>
2023-07-30 21:27:55 +0100

    reinstate inheritance in PWM child classes

--- script_0_10b.py
+++ script_0_10b.py
@@ -7,13 +7,13 @@
 """
 import uasyncio as asyncio
 from machine import Pin
-import array
 from time import ticks_ms, ticks_diff
+from script_0_10 import Queue
 
 
 def encode_id_data(id_, data):
     """ encode id and event as single byte
-        - 0: none; 1: click; 2: hold
+        - event: 0: none; 1: click; 2: hold
     """
     return (id_ << 2) + data
 
@@ -41,7 +41,7 @@ class Button:
 
     async def poll_input(self):
         """ poll button for press or hold events
-            - encoded event includes self.id as least significant bits
+            - encoded event includes id as most significant bits
             - add each event to out_queue
         """
         on_time = 0
@@ -59,67 +59,6 @@ class Button:
                     on_time = time_stamp
                 prev_state = state
             await asyncio.sleep_ms(20)
-
-
-class Queue:
-    """ simple FIFO array of value: type-code
-        - selected type-codes for unsigned int values:
-            'B' 1-byte; 'I' 2-byte; 'L' 4-byte
-        - is_data and is_space events control .wait access
-        - Event.set() "must be called from within a task",
-            hence coros.
-    """
-
-    def __init__(self, type_code, max_len=16):
-        self.max_len = max_len
-        self.queue = array.array(type_code, [0] * max_len)  # unsigned single-byte integer
-        self.head = 0
-        self.next = 0
-        self.is_data = asyncio.Event()
-        self.is_space = asyncio.Event()
-        self.is_space.set()
-
-    async def add(self, item):
-        """ add item to the queue """
-        next_ = self.next
-        self.queue[next_] = item
-        next_ = (next_ + 1) % self.max_len
-        if next_ == self.head:
-            self.is_space.clear()
-        self.next = next_
-        self.is_data.set()
-
-    async def pop(self):
-        """ remove item from the queue """
-        head_ = self.head
-        item = self.queue[head_]
-        head_ = (head_ + 1) % self.max_len
-        if head_ == self.next:
-            self.is_data.clear()
-        self.head = head_
-        self.is_space.set()
-        return item
-
-    @property
-    def q_len(self):
-        """ number of items in the queue """
-        if self.head == self.next:
-            if self.is_data.is_set():
-                n = self.max_len
-            else:
-                n = 0
-        else:
-            n = (self.next - self.head) % self.max_len
-        return n
-
-    def q_print(self):
-        """ print out all queue-item values """
-        print(f'head: {self.head}; next: {self.next}')
-        q_str = '['
-        for i in range(self.max_len):
-            q_str += f'{self.queue[i]}, '
-        q_str = q_str[:-2] + ']'
-        print(q_str)
 
 
 async def button_event(q_in):

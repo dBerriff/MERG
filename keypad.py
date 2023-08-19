b23ab7da394d1bd50d3ad8dbec124e4ea08c5978
dBerriff <berriff@icloud.com>
2023-08-19 22:02:58 +0100

    simplify keypad and parser

--- keypad.py
+++ keypad.py
@@ -1,8 +1,16 @@
 """
     Read matrix keypad with Pi Pico
-    Generic keypad supplied as component in Elegoo kit.
+    Hardware: generic 4-column, 4-row keypad
     Simple "membrane" switches close at each column-to-row intersection
-    Elegoo diagram shows:
+    =======
+    1 2 3 A
+    4 5 6 B
+    7 8 9 C
+    * 0 # D
+    =======
+
+    8 x connectors, left-to-right, from Elegoo diagram:
+    
     0:  1-2-3-A  Rows
     1:  4-5-6-B
     2:  7-8-9-C
@@ -11,101 +19,88 @@
     5:  2-5-8-0
     6:  3-6-9-#
     7:  A-B-C-D
-    In code, coordinate order is (col, row), as for (x, y)
 
-    queue.py and parser.py must be uploaded to the Pi Pico
+    In code, coordinate order is (col, row)
 """
-
+# queue.py and parser.py must be uploaded to the Pico
 from machine import Pin
 import uasyncio as asyncio
-from time import ticks_ms
 from queue import CharBuffer
 from parser import Lexer
 
 
 class SwitchMatrix:
     """ matrix of switched nodes
-        - matrix data returned as linear list index: index = (row * n_cols + col)
-        - m_list as list; array gives marginal or no greater efficiency
+        - matrix data returned as linear list: index = (row * n_cols + col)
     """
     
     def __init__(self, cols, rows):
-        # columns are scanned inputs; rows are outputs set high in sequence
-        self.col_pins = [Pin(pin, mode=Pin.IN, pull=Pin.PULL_DOWN) for pin in cols]
-        self.row_pins = [Pin(pin, mode=Pin.OUT, value=0) for pin in rows]
+        # row outputs set high in sequence, columns scanned as inputs
+        self.col_pins = tuple([Pin(pin, mode=Pin.IN, pull=Pin.PULL_DOWN) for pin in cols])
+        self.row_pins = tuple([Pin(pin, mode=Pin.OUT, value=0) for pin in rows])
         self._list_len = len(cols) * len(rows)
-        self.m_list = [0] * len(self)  # fixed-length list
+        self.m_list = [0] * self._list_len  # fixed-length list
 
     def __len__(self):
+        """ length of matrix as list """
         return self._list_len
 
     def scan_matrix(self):
-        """ scan matrix nodes """
+        """ scan matrix nodes by (col, row) """
         index = 0
         for r_pin in self.row_pins:
             r_pin.high()
             for c_pin in self.col_pins:
                 self.m_list[index] = c_pin.value()
-                index += 1
+                index += 1  # row * n_cols + col
             r_pin.low()
         return self.m_list
 
 
 class KeyPad(SwitchMatrix):
-    """ process SwitchMatrix as matrix of Key objects
+    """ process SwitchMatrix as tuple of Key objects
         - output key-value to Buffer object
         - matrix nodes and key objects matched in (col, row) order
     """
-    key_char_list = (
-        '1', '2', '3', 'A',
-        '4', '5', '6', 'B',
-        '7', '8', '9', 'C',
-        '*', '0', '#', 'D'
-    )
-
-    digits = '0123456789'
-    letters = 'ABCD'
-    symbols = '*#'
+    key_char_list = tuple('123A456B789C*0#D')
+    digits = set('0123456789')
+    letters = set('ABCD')
+    symbols = set('*#')
+    alphanumeric = digits.union(letters)
 
     def __init__(self, cols, rows, buffer):
         super().__init__(cols, rows)
         self.buffer = buffer
         self.key_list = []
         for char in KeyPad.key_char_list:
-            key = Key(char)
-            self.key_list.append(key)
+            self.key_list.append(Key(char))
+        self.key_list = tuple(self.key_list)
  
     async def key_input(self):
-        """ coro: detect key-press in switch matrix
+        """ coro: detect key-presses in switch matrix
             - data producer: put char into buffer
-            - detects multiple presses
+
         """
-        # scan matrix repeatedly
+        scan_interval = 100  # ms - adjust as required
         while True:
-            scan_time = ticks_ms()
             matrix_states = self.scan_matrix()
             for index in range(len(self)):
                 node_state = matrix_states[index]
                 key = self.key_list[index]
-                if node_state == 1 and key.state == 0:
-                    key.state = 1  # key has been pressed
-                    key.time_pressed = scan_time
-                    await self.buffer.put(key.char)
-                elif node_state == 0 and key.state == 1:
-                    key.state = 0  # key has been released
-            await asyncio.sleep_ms(200)  # scan interval
+                if key.state != node_state:
+                    if node_state == 1:
+                        await self.buffer.put(key.char)
+                    key.state = node_state
+            await asyncio.sleep_ms(scan_interval)
 
 
 class Key:
-    """ keypad key-switch
-        - key-press time saved as ticks_ms for future dev
-    """
+    """ keypad key-switch """
 
     def __init__(self, char):
         self._char = char
         self.state = 0
         self.pressed = False
-        self.time_pressed = None
 
     @property
     def char(self):
@@ -117,13 +112,16 @@ class Key:
 
 async def main():
     """ test keypad input and parsing """
-    
+
     async def consumer(lex_):
         """ consume input characters """
-        while True:
+        # tokens returned as t_type: 'integer', 'string' or 'symbol'
+        t_value = None
+        print('consumer() started: enter "DDD" to end')
+        while t_value != 'DDD':
             print('Integer: enter a digit; String: enter a letter:')
             t_type, t_value = await lex_.get_token()
-            print(f'token: {t_type} value: {t_value}')
+            print(f'token: type: {t_type} {type(t_value)}; value: {t_value}')
 
     # KeyPad: RPi Pico pin assignments
     cols = (12, 13, 14, 15)
@@ -135,8 +133,9 @@ async def main():
     prompt = '- Integers start with a number, strings with a letter\n'
     prompt += '- Press # to end integer or string input, * to delete a character\n'
     print(prompt)
-    asyncio.create_task(consumer(lex))
-    await kp.key_input()
+    asyncio.create_task(kp.key_input())
+    await consumer(lex)
+    print('Test complete')
 
 
 if __name__ == '__main__':

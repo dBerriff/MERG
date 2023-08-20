dc1ee6f1b42bc3555236908bdf809d787e49e1f1
dBerriff <berriff@icloud.com>
2023-08-20 06:33:36 +0100

    Update parser.py
    Simplfy

--- parser.py
+++ parser.py
@@ -10,8 +10,7 @@ class Lexer:
     """
 
     def __init__(self, kp_, buffer_):
-        self.kp = kp_
-        self.buffer = buffer_
+        self.get_char = buffer_.get
         self.digits = kp_.digits
         self.letters = kp_.letters
         self.alphanumeric = kp_.alphanumeric
@@ -30,7 +29,7 @@ class Lexer:
             """
             while True:
                 print(f'Entered: {token_value_}')
-                char_ = await self.buffer.get()
+                char_ = await self.get_char()
                 if char_ in char_set:
                     token_value_ += char_
                 elif char_ in self.symbols:
@@ -44,21 +43,21 @@ class Lexer:
                         break
             return token_value_
 
-        char = await self.buffer.get()
+        char = await self.get_char()
 
-        if char in self.kp.digits:
+        if char in self.digits:
             token_type = 'integer'
             token_value = await scan_stream(char, self.digits)
             if token_value:
                 token_value = int(token_value)
             else:
                 token_type = None
-        elif char in self.kp.letters:
+        elif char in self.letters:
             token_type = 'string'
             token_value = await scan_stream(char, self.alphanumeric)
             if not token_value:
                 token_type = None
-        elif char in self.kp.symbols:
+        elif char in self.symbols:
             token_type = 'symbol'
             token_value = char
         else:
